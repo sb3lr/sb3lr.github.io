966207f940c4580765c52f19fc0590ee9b8b1b77
sb3ly <h_3x@outlook.sa>
2025-06-15 23:33:51 +0300

    Fix basename and routes to avoid URL in app

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
+  base: '/madar/',  // تأكد إنها اسم الريبو مع شرطة في البداية والنهاية
   plugins: [react()],
-  base: '/madar/', // المفروض ذا يكون اسم المستودع على حسب كلام gemini 
 });
+
