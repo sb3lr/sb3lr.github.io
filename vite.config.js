8f1bfc9b95e8bd2b35a3e9fdcdeabd7a50dc12f3
Saeed <90979595+sb3ly@users.noreply.github.com>
2025-07-01 16:16:57 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: '/madar/',  // تأكد إنها اسم الريبو مع شرطة في البداية والنهاية
+  base: '/',  // تأكد إنها اسم الريبو مع شرطة في البداية والنهاية
   plugins: [react()],
 });
 
